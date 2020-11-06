5d4e136b4705b5498d4302bc57971d5a151008b2
Pipicallu <thomas.vassallo@gmail.com>
2020-11-06 17:44:11 +0000

    connect flask to mongodb

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask 
+from flask import (
+    Flask, flash, render_template, redirect,
+    request, session, url_for) 
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -11,10 +13,13 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route("/")
-def hello():
-    return "hello world! ... again.."
+mongo = PyMongo(app)
 
+@app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
